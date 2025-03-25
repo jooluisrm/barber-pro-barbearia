d13541dd419609a031d77ea7c3e9142c43b821e8
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-03-25 09:18:44 -0300

    page register feito

--- src/components/loginAndRegister/mainRegister.tsx
+++ src/components/loginAndRegister/mainRegister.tsx
@@ -9,42 +9,69 @@ import { TitlePage } from "./titlePage";
 export const MainRegister = () => {
 
     const [mostrarSenha, setMostrarSenha] = useState(false);
-    
-        const VerSenha = () => {
-            if (!mostrarSenha){
-                setMostrarSenha(true);
-            } else {
-                setMostrarSenha(false);
-            }
+
+    const VerSenha = () => {
+        if (!mostrarSenha) {
+            setMostrarSenha(true);
+        } else {
+            setMostrarSenha(false);
         }
+    }
 
     return (
         <main className="min-w-[450px] mt-20 flex flex-col items-center">
-            <TitlePage title="BarberPro" subtitle="Registre sua barbearia"/>
-            <form className="flex flex-col gap-5">
-                <div>
-                    <label htmlFor="email">E-mail:</label>
-                    <Input id="email" placeholder="Digite seu email" />
-                </div>
-                <div>
-                    <label htmlFor="senha">Senha:</label>
-                    <div className="relative flex items-center">
-                        <Input
-                            id="senha"
-                            type={`${mostrarSenha ? "text" : "password"}`}
-                            placeholder="Digite sua senha"
-                            className="flex-1 pr-10"
-                        />
-                        <div className="absolute right-3" onClick={() => VerSenha()}>
-                            {!mostrarSenha ? <EyeClosed  /> : <Eye  />}
+            <TitlePage title="BarberPro" subtitle="Registre sua barbearia" />
+            <form className="grid grid-cols-1 md:grid-cols-2 gap-10 md:min-w-[700px] mb-5">
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-lg font-bold">Dados Pessoais:</h1>
+                    <div>
+                        <label htmlFor="email">E-mail:</label>
+                        <Input id="email" placeholder="Digite seu email" />
+                    </div>
+                    <div>
+                        <label htmlFor="senha">Senha:</label>
+                        <div className="relative flex items-center">
+                            <Input
+                                id="senha"
+                                type={`${mostrarSenha ? "text" : "password"}`}
+                                placeholder="Digite sua senha"
+                                className="flex-1 pr-10"
+                            />
+                            <div className="absolute right-3" onClick={() => VerSenha()}>
+                                {!mostrarSenha ? <EyeClosed /> : <Eye />}
+                            </div>
                         </div>
                     </div>
+                    <div>
+                        <label htmlFor="celular">Celular:</label>
+                        <Input id="celular" placeholder="Digite seu celular" />
+                    </div>
+                    <div>
+                        <label htmlFor="telefone">Telefone:</label>
+                        <Input id="telefone" placeholder="Digite sua senha" />
+                    </div>
                 </div>
-                <Button className="font-bold">Entrar</Button>
-                <div className="text-sm dark:text-gray-400 text-gray-600">
-                    Não tem uma conta?  <span className="font-bold text-blue-500 cursor-pointer">Cadastre-se aqui</span> e comece agora!
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-lg font-bold">Dados da Barbearia:</h1>
+                    <div>
+                        <label htmlFor="nomeBarbearia">Nome da Barbearia:</label>
+                        <Input id="nomeBarbearia" placeholder="Digite o nome da sua barbearia" />
+                    </div>
+                    <div>
+                        <label htmlFor="endereco">Endereço da Barbearia:</label>
+                        <Input id="endereco" placeholder="ex: Rua Brasil, Centro, 999" />
+                    </div>
+                    <div>
+                        <label htmlFor="latitude">Latitude:</label>
+                        <Input id="latitude" placeholder="Digite a Latitude" />
+                    </div>
+                    <div>
+                        <label htmlFor="lontitude">Lontitude:</label>
+                        <Input id="lontitude" placeholder="Digite a Lontitude" />
+                    </div>
                 </div>
             </form>
+            <Button className="font-bold mb-10 mt-5 min-w-[246px] md:w-full">Criar Barbearia</Button>
         </main>
     );
 }
