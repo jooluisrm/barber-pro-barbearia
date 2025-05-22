e08c3a8a80de3b1c6e13cc1a36a49e13a9227a19
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-22 09:16:33 -0300

    zod feito ao editar barbeiro

--- src/components/barbeiros/contentAlterarDados.tsx
+++ src/components/barbeiros/contentAlterarDados.tsx
@@ -18,6 +18,11 @@ import { Clock } from "lucide-react";
 import { loadItems } from "@/utils/loadItems";
 import { useAuth } from "@/contexts/AuthContext";
 import { useBarberContext } from "@/contexts/BarberContext";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import InputMask from 'react-input-mask'
 
 
 type Props = {
@@ -26,6 +31,16 @@ type Props = {
     setOpen: (e: boolean) => void;
 }
 
+const formSchema = z.object({
+    nome: z.string().min(2, 'Precisa ter minimo 2 caracteres!'),
+    email: z.string().email('E-mail inválido!'),
+    telefone: z
+        .string()
+        .regex(/^\(\d{2}\) \d{5}-\d{4}$/, {
+            message: 'Celular inválido (ex: (11) 91234-5678)',
+        }),
+});
+
 export const ContentAlterarDados = ({ barbeiro, nextPage, setOpen }: Props) => {
     const { barbearia } = useAuth();
     const { setBarbeiros } = useBarberContext();
@@ -33,6 +48,23 @@ export const ContentAlterarDados = ({ barbeiro, nextPage, setOpen }: Props) => {
     const [inputNome, setInputNome] = useState(barbeiro.nome);
     const [inputEmail, setInputEmail] = useState(barbeiro.email);
     const [inputTelefone, setInputTelefone] = useState(barbeiro.telefone);
+
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: { nome: barbeiro.nome, email: barbeiro.email, telefone: barbeiro.telefone },
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const { nome, email, telefone } = values
+        const done = await editBarbeiro(barbeiro.id, {nome, email, telefone});
+
+        if(done) {
+            setOpen(false);
+            await loadItems(barbearia, getBarbeiros, setBarbeiros);
+        }
+        console.log(values);
+    }
 
     const handleEdit = async () => {
         const done = await editBarbeiro(barbeiro.id, { nome: inputNome, email: inputEmail, telefone: inputTelefone });
@@ -51,52 +83,80 @@ export const ContentAlterarDados = ({ barbeiro, nextPage, setOpen }: Props) => {
                 </DialogDescription>
             </AlertDialogHeader>
             <main className="flex flex-col gap-5">
-                <div>
-                    <label htmlFor="nome">Nome Completo:</label>
-                    <Input
-                        id="nome"
-                        placeholder="Digite seu nome completo"
-                        value={inputNome}
-                        onChange={(e) => setInputNome(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="telefone">Telefone/Celular:</label>
-                    <Input
-                        id="telefone"
-                        type="tel"
-                        placeholder="Digite seu novo numero"
-                        value={inputTelefone}
-                        onChange={(e) => setInputTelefone(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">E-mail:</label>
-                    <Input
-                        id="email"
-                        type="email"
-                        placeholder="Digite seu novo email"
-                        value={inputEmail}
-                        onChange={(e) => setInputEmail(e.target.value)}
-                    />
-                </div>
-                <div className="block">
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <Button variant="outline" onClick={() => nextPage()}><Clock /></Button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                Gerenciar Horários
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                        <FormField
+                            control={form.control}
+                            name="nome"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nome Completo:</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Nome do barbeiro" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="telefone"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Celular:</FormLabel>
+                                    <FormControl>
+                                        <InputMask
+                                            mask="(99) 99999-9999"
+                                            value={field.value}
+                                            onChange={field.onChange}
+                                            onBlur={field.onBlur}
+                                        >
+                                            {(inputProps: any) => (
+                                                <Input
+                                                    {...inputProps}
+                                                    inputRef={field.ref} // ESSENCIAL: envia ref correta ao RHF
+                                                    placeholder="Digite seu celular..."
+                                                />
+                                            )}
+                                        </InputMask>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>E-mail:</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="E-mail do barbeiro" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
 
-                <div className="mt-5 flex justify-between">
-                    <AlertDeletar barbeiro={barbeiro} />
-                    <Button className="font-bold" onClick={handleEdit}>Salvar Alterações</Button>
-                </div>
+                        <div className="block">
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <Button variant="outline" onClick={() => nextPage()}><Clock /></Button>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        Gerenciar Horários
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        </div>
+
+                        <div className="mt-5 flex justify-between">
+                            <AlertDeletar barbeiro={barbeiro} />
+                            <Button className="font-bold" type="submit">Salvar Alterações</Button>
+                        </div>
+                    </form>
+                </Form>
             </main>
         </>
     );
